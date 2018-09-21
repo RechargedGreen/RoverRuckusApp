8c04b104caf58b8513312eb6a3e0c047a3174535
David Lukens <davidqcy02@gmail.com>
2018-09-20 20:11:25 -0500

    started lift class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/botA/hardware/Lift.kt
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.botA.hardware
+
+import com.david.rechargedkotlinlibrary.internal.hardware.devices.sensors.OptimumAnalogInput
+import com.david.rechargedkotlinlibrary.internal.hardware.devices.sensors.RevTouchSensor
+import com.david.rechargedkotlinlibrary.internal.hardware.management.MTSubsystem
+
+class Lift(val robot: BotAHardwareClass) : MTSubsystem {
+    private var state: State
+        set(value) {
+            controlState = ControlState.AUTO
+            state = value
+        }
+        get() = state
+
+
+    private val downSensor = RevTouchSensor(robot.getHub(0), 0)
+    private val upSensor = RevTouchSensor(robot.getHub(0), 0)
+
+    private val motor1 = robot.hMap.dcMotor.get("lift1")
+    private val motor2 = robot.hMap.dcMotor.get("lift1")
+
+    private fun internalSetMotorPowers(power: Double) {
+        motor1.power = power
+        motor2.power = power
+    }
+
+    private var openLoop = 0.0
+
+    private enum class ControlState {
+        MANUAL_SAFE,
+        MANUAL_DANGER,
+        AUTO
+    }
+
+    private var controlState = ControlState.AUTO
+
+    private fun setOpenLoopPower(power: Double, useFailSafes: Boolean = true) {
+        openLoop = power
+        controlState = if (useFailSafes) ControlState.MANUAL_SAFE else ControlState.MANUAL_DANGER
+    }
+
+    enum class State {
+        DOWN,
+        CARGO_SCORING,
+        HANG,
+        DEPLOY,
+        LATCH,
+    }
+
+    override fun update() {
+    }
+
+    fun isFullyDown() = downSensor.pressed()
+    fun isFullyUp() = upSensor.pressed()
+
+    override fun start() {
+    }
+
+    init {
+        robot.thread.addSubsystem(this)
+        state = State.DOWN
+    }
+}
