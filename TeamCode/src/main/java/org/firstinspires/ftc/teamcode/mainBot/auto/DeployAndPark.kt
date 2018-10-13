48db88a4325d5883296fd3d76139d497e6bb4e33
David Lukens <davidqcy02@gmail.com>
2018-10-13 18:13:35 -0500

    converted DeployAndPark to encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/DeployAndPark.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/DeployAndPark.kt
@@ -6,8 +6,11 @@ import org.firstinspires.ftc.teamcode.mainBot.hardware.HardwareClass
 class DeployAndPark : FluidAuto<HardwareClass>({ opMode -> HardwareClass(opMode) }){
     override fun run() {
         robot.lift.deploy()
+
+        robot.drive.resetEncoders()
         robot.drive.startFollowingAngle_setConstants(angle = 0.0)
-        sleep(3000)
+        waitWhile { robot.drive.leftTicks() + robot.drive.rightTicks() < 5000 }
+
         robot.drive.stop()
     }
 }
