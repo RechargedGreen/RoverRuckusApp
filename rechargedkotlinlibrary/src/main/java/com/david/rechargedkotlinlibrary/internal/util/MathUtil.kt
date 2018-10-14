3d4f16441b00fff5941cf7e24902c98cee9214ad
David Lukens <davidqcy02@gmail.com>
2018-10-14 14:44:11 -0500

    added angle norm

--- rechargedkotlinlibrary/src/main/java/com/david/rechargedkotlinlibrary/internal/util/MathUtil.kt
+++ rechargedkotlinlibrary/src/main/java/com/david/rechargedkotlinlibrary/internal/util/MathUtil.kt
@@ -1,9 +1,19 @@
 package com.david.rechargedkotlinlibrary.internal.util
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+
 /**
  * Created by David Lukens on 8/8/2018.
  */
 object MathUtil {
     const val TAU = 2.0 * Math.PI
     fun radiansToInches(radians: Double, radius: Double) = TAU * radians * radius
+    fun norm(angle: Double, angleUnit: AngleUnit = AngleUnit.DEGREES): Double {
+        var angle = if(angleUnit == AngleUnit.RADIANS) angle else Math.toRadians(angle)
+        angle = (angle % TAU)
+        angle = (angle + TAU) % TAU
+        if (angle > Math.PI)
+            angle -= TAU
+        return if(angleUnit == AngleUnit.RADIANS) angle else Math.toDegrees(angle)
+    }
 }
