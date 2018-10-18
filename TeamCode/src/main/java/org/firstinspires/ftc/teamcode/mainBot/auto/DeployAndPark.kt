c873a6bf79f69e0abc9f3d3fb2868e6133445470
David Lukens <davidqcy02@gmail.com>
2018-10-18 15:22:37 -0500

    changed the auto groups in OpModeGroups

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/DeployAndPark.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/DeployAndPark.kt
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.mainBot.auto
 
 import com.david.rechargedkotlinlibrary.internal.opMode.FluidAuto
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.firstinspires.ftc.teamcode.mainBot.hardware.HardwareClass
+import org.firstinspires.ftc.teamcode.mainBot.misc.OpModeGroups
 
+@Autonomous(group = OpModeGroups.AUTO)
 class DeployAndPark : FluidAuto<HardwareClass>({ opMode -> HardwareClass(opMode) }){
     override fun run() {
         robot.lift.deploy()
