e89a4a69649332675036c6fdf8571b8a21a9cde2
David Lukens <davidqcy02@gmail.com>
2018-10-22 21:26:47 -0500

    fixed sample positions names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/data/FIELD_POSITIONS.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/data/FIELD_POSITIONS.kt
@@ -9,15 +9,15 @@ object FIELD_POSITIONS {
 
     @JvmField var DEPOT_HARD_ALIGN = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
 
-    @JvmField var GOLD_SAMPLE_FROM_LANDER_GENERAL = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
-    @JvmField var GOLD_SAMPLE_FROM_LANDER_LEFT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
-    @JvmField var GOLD_SAMPLE_FROM_LANDER_CENTER = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
-    @JvmField var GOLD_SAMPLE_FROM_LANDER_RIGHT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
+    @JvmField var SILVER_SAMPLE_FROM_LANDER_GENERAL = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
+    @JvmField var SILVER_SAMPLE_FROM_LANDER_LEFT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
+    @JvmField var SILVER_SAMPLE_FROM_LANDER_CENTER = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
+    @JvmField var SILVER_SAMPLE_FROM_LANDER_RIGHT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
 
-    @JvmField var SILVER_SAMPLE_FROM_DEPOT_GENERAL = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
-    @JvmField var SILVER_SAMPLE_FROM_DEPOT_LEFT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
-    @JvmField var SILVER_SAMPLE_FROM_DEPOT_CENTER = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
-    @JvmField var SILVER_SAMPLE_FROM_DEPOT_RIGHT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
+    @JvmField var GOLD_SAMPLE_FROM_DEPOT_GENERAL = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
+    @JvmField var GOLD_SAMPLE_FROM_DEPOT_LEFT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
+    @JvmField var GOLD_SAMPLE_FROM_DEPOT_CENTER = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
+    @JvmField var GOLD_SAMPLE_FROM_DEPOT_RIGHT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
 
     @JvmField var ALIGN_WALL_FOLLOW_SILVER_SAMPLE_TO_DEPOT = Pose2d(0.0, 0.0, 0.0)// todo change for actual position
 }
