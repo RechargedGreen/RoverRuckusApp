cf7c0f8cb07bb79fbd0b2ddc5d43b95fd468481c
David Lukens <davidqcy02@gmail.com>
2018-10-26 16:12:35 -0500

    added DriveEncodersTester to op mode manager

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/diagnostics/DriveEncodersTester.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/diagnostics/DriveEncodersTester.kt
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.mainBot.diagnostics
 
 import com.david.rechargedkotlinlibrary.internal.opMode.PracticeTeleOp
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.mainBot.hardware.HardwareClass
+import org.firstinspires.ftc.teamcode.mainBot.misc.OpModeGroups
 
+@TeleOp(group = OpModeGroups.TELE_DIAGNOSTICS)
 class DriveEncodersTester : PracticeTeleOp<HardwareClass>({ opMode -> HardwareClass(opMode) }){
     override fun onLoop() {
         telemetry.addData("lf", robot.getHub(0).getEncoder(0))
