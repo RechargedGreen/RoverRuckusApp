57a60d8ed48950218d77c4b216aaa213faa7a836
David Lukens <davidqcy02@gmail.com>
2018-10-29 20:51:41 -0500

    Practice Tele lift doesn't use fail safes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/teleOp/Practice.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/teleOp/Practice.kt
@@ -15,7 +15,8 @@ open class Practice : PracticeTeleOp<HardwareClass>({ opMode -> HardwareClass(op
         val r = c1.ry * (1.0 - c1.rt)
         robot.drive.openLoopPowerWheels(if(l.absoluteValue > deadBand) l else 0.0, if(r.absoluteValue > deadBand) r else 0.0)
         val lift = c2.ly
-        robot.lift.setOpenLoopPower(if(lift.absoluteValue > deadBand) lift else 0.0)
+        robot.lift.setOpenLoopPower(if(lift.absoluteValue > deadBand) lift else 0.0, useFailSafes = false)
+        telemetry.addData("lift power", lift)
         //robot.lift.state = if(gamepad1.right_bumper) Lift.State.UP else Lift.State.DOWN
     }
 
