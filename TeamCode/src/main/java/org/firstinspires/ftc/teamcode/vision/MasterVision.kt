73f5bc78b8d41c6cf4fd94af4e2d913fb3b8b6ba
David Lukens <davidqcy02@gmail.com>
2018-10-31 17:04:51 -0500

    started vision api

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/MasterVision.kt
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.vision
+
+/**
+ * Created by David Lukens on 10/31/2018.
+ */
+
+class MasterVision {
+    val tfLite = TFLite(this)
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/TFLite.kt
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.vision
+
+/**
+ * Created by David Lukens on 10/31/2018.
+ */
+class TFLite(master:MasterVision) {
+    fun getLastKnownSampleOrder():SampleRandomizedPositions{
+        return SampleRandomizedPositions.UNKNOWN
+    }
+}
