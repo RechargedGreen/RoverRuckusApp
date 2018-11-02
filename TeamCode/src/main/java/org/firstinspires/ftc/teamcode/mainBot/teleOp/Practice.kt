8b9e8c2a9108234a25f0fd61fe96cc021d6d6674
David Lukens <davidqcy02@gmail.com>
2018-11-01 22:16:59 -0500

    made good lift teleop code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/teleOp/Practice.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/teleOp/Practice.kt
@@ -12,15 +12,33 @@ import kotlin.math.absoluteValue
 open class Practice : PracticeTeleOp<HardwareClass>({ opMode -> HardwareClass(opMode) }) {
     val deadBand = 0.05
     val liftFailSafesToggle = BooleanToggle(true)
+    private enum class Mode{
+        MANUAL,
+        AUTO
+    }
+
+    private var liftMode = Mode.AUTO
+
     override fun onLoop() {
         val l = c1.ly * (1.0 - c1.lt)
         val r = c1.ry * (1.0 - c1.rt)
         robot.drive.openLoopPowerWheels(if(l.absoluteValue > deadBand) l else 0.0, if(r.absoluteValue > deadBand) r else 0.0)
+
+        liftFailSafesToggle.update(c2.x)
         val lift = c2.ly
-        robot.lift.setOpenLoopPower(if(lift.absoluteValue > deadBand) lift else 0.0, useFailSafes = liftFailSafesToggle.update(gamepad1.x))
+        if(lift.absoluteValue > deadBand || !liftFailSafesToggle.toggled())
+            liftMode = Mode.MANUAL
+        if(c2.y && liftFailSafesToggle.toggled()) {
+            liftMode = Mode.AUTO
+        }
+        if(liftMode == Mode.MANUAL)
+            robot.lift.setOpenLoopPower(if(lift.absoluteValue > deadBand) lift else 0.0, useFailSafes = liftFailSafesToggle.toggled())
+        else
+            robot.lift.state = if(c1.rb) Lift.State.UP else Lift.State.DOWN
+
         telemetry.addData("lift power", lift)
         telemetry.addData("using liftFailSafes", liftFailSafesToggle.toggled())
-        //robot.lift.state = if(gamepad1.right_bumper) Lift.State.UP else Lift.State.DOWN
+        telemetry.addData("lift mode", liftMode)
     }
 
     companion object {
