2d71dd09fff214a5a7c84cc16186d927d7636c4f
David Lukens <davidqcy02@gmail.com>
2018-11-01 19:41:09 -0500

    changed constructor for RevTouchSensor

--- rechargedkotlinlibrary/src/main/java/com/david/rechargedkotlinlibrary/internal/hardware/devices/sensors/RevTouchSensor.kt
+++ rechargedkotlinlibrary/src/main/java/com/david/rechargedkotlinlibrary/internal/hardware/devices/sensors/RevTouchSensor.kt
@@ -1,12 +1,9 @@
 package com.david.rechargedkotlinlibrary.internal.hardware.devices.sensors
 
-import com.david.rechargedkotlinlibrary.internal.hardware.devices.RevHub
-
 /**
  * Created by David Lukens on 8/9/2018.
  */
-class RevTouchSensor(HUB: RevHub, PORT: Int) {
-    private val delegate = OptimumDigitalInput(HUB, PORT)
+class RevTouchSensor(val delegate:OptimumDigitalInput) {
     fun pressed() = !released()
     fun released() = delegate.state()
 }
