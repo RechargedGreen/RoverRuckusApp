c33221ef94c5b7fd54c004877f5a8f78ba594bc3
David Lukens <davidqcy02@gmail.com>
2018-11-03 14:51:38 -0500

    added instructions to

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/tuner/PIDCorrectionTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/tuner/PIDCorrectionTest.kt
@@ -7,14 +7,18 @@ import org.firstinspires.ftc.teamcode.mainBot.hardware.HardwareClass
 
 class PIDCorrectionTest : PracticeTeleOp<HardwareClass>( {opMode -> HardwareClass(opMode) } ){
     override fun onLoop() {
-        if(c1.a)
+        if(c1.y)
             robot.drive.startFollowingAngle(angle = 0.0, controller = PIDController(coeffs = PIDCoefficients(0.1, 0.0, 0.0)), power = 0.0)
-        if(c1.a)
+        if(c1.x)
             robot.drive.startFollowingAngle(angle = 0.0, controller = PIDController(coeffs = PIDCoefficients(0.0, 0.001, 0.0)), power = 0.0)
         if(c1.a)
             robot.drive.startFollowingAngle(angle = 0.0, controller = PIDController(coeffs = PIDCoefficients(0.0, 0.0, 0.5)), power = 0.0)
         if(c1.b)
             robot.drive.stop()
+        telemetry.addLine("y for p")
+        telemetry.addLine("x for i")
+        telemetry.addLine("a for d")
+        telemetry.addLine("b to stop")
         packet.put("error", robot.drive.lastAngleFollowerError)
     }
 }
