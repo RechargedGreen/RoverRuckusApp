d02ede294e39b0f0ea607d5a0f7e27c3bb4563fb
David Lukens <davidqcy02@gmail.com>
2018-11-15 16:53:39 -0600

    added @Autonomous annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/Deploy_SingleSample_Park_Temp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/Deploy_SingleSample_Park_Temp.kt
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.mainBot.auto
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+
 /**
  * Created by David Lukens on 11/15/2018.
  */
-
+@Autonomous
 class Deploy_SingleSample_Park_Temp : Deploy_SingleSample_Temp(){
     override fun run(){
         park = true
