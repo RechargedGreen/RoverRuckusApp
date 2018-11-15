33b7aeae7eeb81870c5c1ffafb9f1052c32e217a
David Lukens <davidqcy02@gmail.com>
2018-11-14 19:09:41 -0600

    added PID tuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/tuner/AngleFollowTuner.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/tuner/AngleFollowTuner.kt
@@ -1,17 +1,69 @@
 package org.firstinspires.ftc.teamcode.mainBot.tuner
 
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.config.Config
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.david.rechargedkotlinlibrary.internal.hardware.PIDController
+import com.david.rechargedkotlinlibrary.internal.opMode.FluidAuto
 import com.david.rechargedkotlinlibrary.internal.opMode.PracticeTeleOp
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.PIDCoefficients
 import org.firstinspires.ftc.teamcode.mainBot.hardware.DriveTerrain
 import org.firstinspires.ftc.teamcode.mainBot.hardware.HardwareClass
+import org.firstinspires.ftc.teamcode.mainBot.misc.OpModeGroups
 
-class AngleFollowTuner:PracticeTeleOp<HardwareClass>({ opMode -> HardwareClass(opMode) }){
-    override fun onLoop() {
-        telemetry.addLine("press a to start following angle")
-        if(gamepad1.a)
-            robot.drive.startFollowingAngle_setConstants(DriveTerrain.AngleFollowSpeeds.FAST, 0.0)
-        if (gamepad1.b)
-            robot.drive.stop()
-        telemetry.addLine("press b to stop")
-        packet.put("error", robot.drive.lastAngleFollowerError)
+@TeleOp(group = OpModeGroups.TELE_TUNERS)
+@Config
+class AngleFollowTuner:FluidAuto<HardwareClass>({ opMode -> HardwareClass(opMode) }) {
+
+    companion object {
+        @JvmField
+        var currKP = 0.0
+        @JvmField
+        var currKI = 0.0
+        @JvmField
+        var currKD = 0.0
+        @JvmField
+        var currVel = 0.0
+        @JvmField
+        var currTarget = 0.0
+    }
+
+    var following = false
+
+    var lastKP = 0.0
+    var lastKI = 0.0
+    var lastKD = 0.0
+    var lastVel = 0.0
+    var lastTarget = 0.0
+    override fun run() {
+        val dash = FtcDashboard.getInstance()
+        val packet = TelemetryPacket()
+
+        loop {
+            if (gamepad1.b) {
+                robot.drive.stop()
+                following = false
+            }
+            val kP = currKP
+            val kI = currKI
+            val kD = currKD
+            val vel = currVel
+            val target = currTarget
+            if (lastKP != kP || lastKI != kI || lastKD != kD || vel != currVel || lastTarget != target) {
+                lastKP = kP
+                lastKI = kI
+                lastKD = kD
+                lastVel = vel
+                lastTarget = target
+                robot.drive.startFollowingAngle(PIDController(PIDCoefficients(kP, kI, kD)), power = vel, angle = target)
+                following = true
+            }
+
+            packet.put("error", if (following) robot.drive.lastAngleFollowerError else 0.0)
+            packet.addLine("Following: $following")
+        }
+
+        dash.sendTelemetryPacket(packet)
     }
 }
