28ddd4a9dfe02f9594da1981fa6ae86e9d40dd26
David Lukens <davidqcy02@gmail.com>
2018-11-18 16:22:11 -0600

    removed unused autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/RR2Auto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/RR2Auto.kt
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.mainBot.auto
 
+import com.acmerobotics.dashboard.config.Config
 import com.david.rechargedkotlinlibrary.internal.opMode.FluidAuto
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.teamcode.mainBot.hardware.HardwareClass
@@ -8,7 +9,12 @@ import org.firstinspires.ftc.teamcode.vision.SampleRandomizedPositions
 /**
  * Created by David Lukens on 11/18/2018.
  */
+@Config
 abstract class RR2Auto(val angleAtStart:Double) : FluidAuto<HardwareClass>({ opMode -> HardwareClass(opMode) }){
+    companion object {
+        @JvmField var GOLD_HANG_ANGLE = -45.0
+        @JvmField var SILVER_HANG_ANGLE = -135.0
+    }
     var ORDER = SampleRandomizedPositions.UNKNOWN
     override fun tillStart() {
         ORDER = robot.vision.tfLite.lastKnownSampleOrder
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/hardware/SuperSystem.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/hardware/SuperSystem.kt
@@ -1,10 +1,7 @@
 package org.firstinspires.ftc.teamcode.mainBot.hardware
 
-import com.david.rechargedkotlinlibrary.internal.hardware.devices.CachedBlinkenLED
 import com.david.rechargedkotlinlibrary.internal.hardware.management.MTSubsystem
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver
-import org.firstinspires.ftc.teamcode.data.FIELD_POSITIONS
-import org.firstinspires.ftc.teamcode.vision.SampleRandomizedPositions
 
 class SuperSystem(val robot: HardwareClass) : MTSubsystem {
     val blinken = robot.hMap.get(RevBlinkinLedDriver::class.java, "blinken") //as CachedBlinkenLED
@@ -27,44 +24,4 @@ class SuperSystem(val robot: HardwareClass) : MTSubsystem {
 
     override fun update() = blinken.setPattern(if (120.0 - robot.opMode.runtime.seconds() < 10.0) hangTimePattern else normalPattern)
     override fun start() {}
-
-    enum class SampleSituation {
-        LANDER_SILVER,
-        LANDER_GOLD,
-        DEPOT_GOLD
-    }
-
-    fun sample(situation: SampleSituation) {
-        val order = robot.vision.tfLite.lastKnownSampleOrder
-        when (situation) {
-            SampleSituation.LANDER_SILVER -> {
-                robot.drive.waitOnTrajectory(trajectory = robot.drive.trajectoryBuilder()
-                        .splineTo(FIELD_POSITIONS.SILVER_SAMPLE_FROM_LANDER_GENERAL)
-                        .build()
-                )
-                if (order != SampleRandomizedPositions.UNKNOWN) {
-                    robot.drive.waitOnTrajectory(trajectory = robot.drive.trajectoryBuilder()
-                            .splineTo(when (order) {
-                                          SampleRandomizedPositions.UNKNOWN -> throw Exception("Entered unexpected case while sampling $situation")
-                                          SampleRandomizedPositions.LEFT    -> FIELD_POSITIONS.SILVER_SAMPLE_FROM_LANDER_LEFT
-                                          SampleRandomizedPositions.CENTER  -> FIELD_POSITIONS.SILVER_SAMPLE_FROM_LANDER_CENTER
-                                          SampleRandomizedPositions.RIGHT   -> FIELD_POSITIONS.SILVER_SAMPLE_FROM_LANDER_RIGHT
-                                      })
-                            .build())
-                    robot.intake.collectSample()
-                    robot.opMode.sleep(2000)// sleep for testing until sample collection is done
-                }
-                robot.drive.waitOnTrajectory(trajectory = robot.drive.trajectoryBuilder()
-                        .turnTo(FIELD_POSITIONS.ANGLE_BEFORE_WALL_ALIGN_SPLINE_SILVER_SAMPLE_TO_DEPOT)
-                        .splineTo(FIELD_POSITIONS.ALIGN_WALL_FOLLOW_SILVER_SAMPLE_TO_DEPOT)
-                        .build())
-            }
-            SampleSituation.LANDER_GOLD   -> {
-
-            }
-            SampleSituation.DEPOT_GOLD    -> {
-
-            }
-        }
-    }
 }
