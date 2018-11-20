79a449846742124bcf3083f9f4d1d7d29cdc876b
David Lukens <davidqcy02@gmail.com>
2018-11-20 12:38:52 -0600

    fixed bad rename of DeploySlowSampleBackup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/DeploySlowSampleBackup.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/DeploySlowSampleBackup.kt
@@ -3,6 +3,6 @@ package org.firstinspires.ftc.teamcode.mainBot.auto
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 
 @Autonomous
-open class Deploy_SingleSampleBackup_Temp : RR2Auto(StartingPositions.ANY_HANG) {
+open class DeploySlowSampleBackup : RR2Auto(StartingPositions.ANY_HANG) {
     override fun postDeploy() = sample(SampleCollectionType.LANDER_DRIVE_SLOW_BACKUP)
 }
