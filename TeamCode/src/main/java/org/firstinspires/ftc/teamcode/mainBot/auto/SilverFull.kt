e7a7abf42307ef836a137968d3e679b81616c345
David Lukens <davidqcy02@gmail.com>
2018-12-13 15:20:51 -0600

    run lift up early in silver auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/SilverFull.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/SilverFull.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.mainBot.auto
 import com.david.rechargedkotlinlibrary.internal.hardware.driveTerrain.DiffDrive
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.firstinspires.ftc.teamcode.mainBot.hardware.DriveTerrain
+import org.firstinspires.ftc.teamcode.mainBot.hardware.Lift
 import org.firstinspires.ftc.teamcode.mainBot.misc.OpModeGroups
 
 @Autonomous(group = OpModeGroups.MAIN_AUTO)
@@ -11,6 +12,7 @@ class SilverFull : RR2Auto(StartingPositions.SILVER_HANG){
         sample(SampleCollectionType.LANDER_EXTENSION_SILVER)
         silverSampleWallLinup()
         robot.drive.startFollowingAngle_setConstants(DriveTerrain.AngleFollowSpeeds.PARK, CompassDirection.SOUTH.degrees, true, DiffDrive.AnglePIDType.STRAIGHT)
+        robot.lift.state = Lift.State.UP
         robot.sensors.lineDetector.reset()
         waitTill { robot.sensors.lineDetector.hasHit }
         robot.drive.stop()
