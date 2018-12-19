b8027a3ca3ab6102da24f0972bcbf34c663eb5f4
David Lukens <davidqcy02@gmail.com>
2018-12-18 20:43:11 -0600

    add note for self on parking issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/SilverFull.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/auto/SilverFull.kt
@@ -11,7 +11,7 @@ class SilverFull : RR2Auto(StartingPositions.SILVER_HANG){
         silverSampleWallLinup()
         intoDepotSilver()
         teamMarker(true)
-        robot.drive.pidTurn(CompassDirection.SOUTH.degrees + intoWallOffset)
+        robot.drive.pidTurn(CompassDirection.SOUTH.degrees + intoWallOffset)// not parking issue was with the turn not completing
         robot.drive.deadReckonPID(1000, CompassDirection.SOUTH.degrees + intoWallOffset, DriveTerrain.AngleFollowSpeeds.SLOW)
         park(CompassDirection.SOUTH.degrees)
     }
