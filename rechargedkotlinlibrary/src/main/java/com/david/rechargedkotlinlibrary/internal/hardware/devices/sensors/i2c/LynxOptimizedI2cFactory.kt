2569d640329bd917d216ab008c2210878510f4a1
David Lukens <davidqcy02@gmail.com>
2019-01-23 17:48:56 -0600

    added LynxOptimizedI2cFactory with createLynxEmbeddedIMU and createLynxI2cColorRangeSensor

---
+++ rechargedkotlinlibrary/src/main/java/com/david/rechargedkotlinlibrary/internal/hardware/devices/sensors/i2c/LynxOptimizedI2cFactory.kt
@@ -0,0 +1,23 @@
+package com.david.rechargedkotlinlibrary.internal.hardware.devices.sensors.i2c
+
+import com.qualcomm.hardware.lynx.LynxEmbeddedIMU
+import com.qualcomm.hardware.lynx.LynxI2cColorRangeSensor
+import com.qualcomm.hardware.lynx.LynxModule
+import com.qualcomm.hardware.lynx.commands.core.LynxFirmwareVersionManager
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImplOnSimple
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchSimple
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil
+
+/**
+ * Created by David Lukens on 1/23/2019.
+ * inspired by https://github.com/acmerobotics/road-runner-quickstart/blob/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/LynxOptimizedI2cFactory.java
+ */
+object LynxOptimizedI2cFactory {
+    private class BetterI2cDeviceSynchImplOnSimple(simple: I2cDeviceSynchSimple, isSimpleOwned: Boolean) : I2cDeviceSynchImplOnSimple(simple, isSimpleOwned) {
+        override fun setReadWindow(window: I2cDeviceSynch.ReadWindow?) {}
+    }
+    fun createLynxI2cDeviceSynch(module:LynxModule, bus:Int):I2cDeviceSynch = BetterI2cDeviceSynchImplOnSimple(LynxFirmwareVersionManager.createLynxI2cDeviceSynch(AppUtil.getDefContext(), module, bus), true)
+    fun createLynxEmbeddedIMU(module:LynxModule, bus: Int) = LynxEmbeddedIMU(createLynxI2cDeviceSynch(module, bus))
+    fun createLynxI2cColorRangeSensor(module:LynxModule, bus: Int) = LynxI2cColorRangeSensor(createLynxI2cDeviceSynch(module, bus))
+}
