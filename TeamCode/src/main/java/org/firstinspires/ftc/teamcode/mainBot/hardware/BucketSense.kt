f2f82ddab040b0e83cf5f79d78ebee090d78bee9
David Lukens <davidqcy02@gmail.com>
2019-04-24 12:19:25 -0500

    switch bucket to tof

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/hardware/BucketSense.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainBot/hardware/BucketSense.kt
@@ -1,18 +1,26 @@
 package org.firstinspires.ftc.teamcode.mainBot.hardware
 
 import com.acmerobotics.dashboard.config.Config
+import com.qualcomm.robotcore.hardware.DistanceSensor
 import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 
 @Config
 class BucketSense(hMap: HardwareMap) {
-
     companion object {
         @JvmField
-        var threshold = 100000000 // 50000 was old bucket
+        var leftThreshold = 6.0
+        @JvmField
+        var rightThreshold = 6.0
     }
 
-    private val left = hMap.colorSensor.get("leftBucketColor")
-    private val right = hMap.colorSensor.get("rightBucketColor")
+    private val leftDistanceSensor = hMap.get(DistanceSensor::class.java, "leftBucketColor")
+    private val rightDistanceSensor = hMap.get(DistanceSensor::class.java, "rightBucketColor")
+    //private val left = hMap.colorSensor.get("leftBucketColor")
+    //private val right = hMap.colorSensor.get("rightBucketColor")
+
+    var leftDistance = 10000.0
+    var rightDistance = 10000.0
 
     var leftRed = 0
     var leftGreen = 0
@@ -31,19 +39,30 @@ class BucketSense(hMap: HardwareMap) {
         //leftRed = left.red()
         //leftGreen = left.green()
         //leftBlue = left.blue()
-        leftAlpha = left.alpha()
-        leftARGB = left.argb()
+
+        //leftAlpha = left.alpha()
+        //eftARGB = left.argb()
 
         //rightRed = right.red()
         //rightGreen = right.green()
         //rightBlue = right.blue()
-        rightAlpha = right.alpha()
-        rightARGB = right.argb()
+
+        //rightAlpha = right.alpha()
+        //rightARGB = right.argb()
+
+        leftDistance = leftDistanceSensor.getDistance(DistanceUnit.CM)
+        rightDistance = rightDistanceSensor.getDistance(DistanceUnit.CM)
     }
 
     @Throws(InterruptedException::class)
+    fun left() = leftDistance < leftThreshold
+
+    @Throws(InterruptedException::class)
+    fun right() = rightDistance < rightThreshold
+
+    /*@Throws(InterruptedException::class)
     fun left() = leftARGB > threshold
 
     @Throws(InterruptedException::class)
-    fun right() = rightARGB > threshold
+    fun right() = rightARGB > threshold*/
 }
